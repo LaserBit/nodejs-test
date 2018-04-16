47298e48f2783f387e87807faefbd0f8931ebc99
LaserBit <laserbit@fake.bit>
2018-04-16 17:39:16 +0900

    add form

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ var routes = require('./routes/index');
 var bodyParser = require('body-parser');
 var app = express();
 
+// テンプレートエンジンを EJS に設定
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// 静的ファイルは無条件に公開
 app.use(express.static(path.join(__dirname, 'public')));
+// ルーティング設定
 app.use('/', routes);
 
+// サーバーをポート 8080 で起動
 app.listen(process.env.PORT || 8080, function () {
   console.log('listening on port 8080');
 });
