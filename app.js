1b52fabd57e4349bdacdc4f07863ed2b13d83d0a
LaserBit <laserbit@fake.bit>
2018-04-16 17:16:13 +0900

    fix file name

--- app.js
+++ app.js
@@ -11,5 +11,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 app.listen(process.env.PORT || 8080, function () {
-  console.log('listening on port 3000');
+  console.log('listening on port 8080');
 });
